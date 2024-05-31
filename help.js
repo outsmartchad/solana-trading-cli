695a28f1724948eaff010dfd28c8c35d185c2976
chiwangso <chiwang1712@gmail.com>
2024-05-31 23:46:05 +0800

    update help.js

--- help.js
+++ help.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 // Read the contents of the file
-fs.readFile("./readme.txt", "utf8", (error, data) => {
+fs.readFile("./CLI_Doc.txt", "utf8", (error, data) => {
   if (error) {
     console.error("❌ Error reading the help file");
     process.exit(1);
