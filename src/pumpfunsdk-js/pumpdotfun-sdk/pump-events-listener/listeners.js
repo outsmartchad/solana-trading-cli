b07613c71b3f58913a6f4a74d8fd5323d6076e2c
chiwangso <chiwang1712@gmail.com>
2024-07-08 11:17:08 +0800

    add pump.fun events listener

--- src/pumpfunsdk-js/pumpdotfun-sdk/pump-events-listener/listeners.js
+++ src/pumpfunsdk-js/pumpdotfun-sdk/pump-events-listener/listeners.js
@@ -1,49 +1,46 @@
 const {connection} = require("../../../helpers/config");
+const { Keypair, Connection } =  require("@solana/web3.js");
+const {PumpFunSDK} =require ("pumpdotfun-sdk");
+const { AnchorProvider } = require ("@coral-xyz/anchor");
 
-import { Keypair, Connection } from "@solana/web3.js";
-import {PumpFunSDK} from "pumpdotfun-sdk";
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
-import { AnchorProvider } from "@coral-xyz/anchor";
-
-const getProvider =()=>{
-    const wallet = new NodeWallet(Keypair.generate());
+function getProvider(){
+    const wallet = Keypair.generate();
     const provider = new AnchorProvider(connection, wallet, {
         commitment: "finalized",
     });
     return provider;
 }
 
-export const subscribeToCompleteBondingCurveEvent = async (sdk) => {
+async function subscribeToCompleteBondingCurveEvent(sdk){
     const completeEventId = sdk.addEventListener("completeEvent", (event, slot, signature) => {
         console.log("completeEvent", event, slot, signature);
     });
     console.log("Subscribed to completeEvent with ID:", completeEventId);
 }
-export const subscribeToCreatePumpTokenEvent = async (sdk) => {
+async function subscribeToCreatePumpTokenEvent(sdk){
     const createEventId = sdk.addEventListener("createEvent", (event, slot, signature) => {
         console.log("createEvent", event, slot, signature);
         console.log("mint pubkey", event.mint.toBase58())
     });
     console.log("Subscribed to createEvent with ID:", createEventId);
 }
-export const subscribeToTradeEvent = async (sdk) => {
+async function subscribeToTradeEvent(sdk){
     const tradeEventId = sdk.addEventListener("tradeEvent", (event, slot, signature) => {
         console.log("tradeEvent", event, slot, signature);
     });
     console.log("Subscribed to tradeEvent with ID:", tradeEventId);
 }
-
-const main = async () => {
+async function main(){
     try {
-      const provider = getProvider();
-      const sdk = new PumpFunSDK(provider);
-  
-      // Set up event listeners
-      await subscribeToCreatePumpTokenEvent(sdk);
+        const provider = getProvider();
+        const sdk = new PumpFunSDK(provider);
+        
+        // Set up event listeners
+        await subscribeToCreatePumpTokenEvent(sdk);
     } catch (error) {
-      console.error("An error occurred:", error);
+        console.error("An error occurred:", error);
     }
-  };
-  
+}
+main();
 
-  
+module.exports = {subscribeToCompleteBondingCurveEvent, subscribeToCreatePumpTokenEvent, subscribeToTradeEvent, getProvider};
