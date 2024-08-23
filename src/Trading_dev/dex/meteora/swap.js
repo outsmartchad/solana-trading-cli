93f58eee05de8c34daf794d56c2d5924b550de4e
chiwangso <chiwang1712@gmail.com>
2024-08-23 15:15:04 +0800

    updated package.json

--- src/Trading_dev/dex/meteora/swap.js
+++ src/Trading_dev/dex/meteora/swap.js
@@ -1,10 +1,10 @@
-const { Amm, IDL } = require("./idl.js");
-const { Connection, PublicKey, Keypair } = require("@solana/web3.js");
+const DLMM = require('@meteora-ag/dlmm');
+const { PublicKey, Keypair } = require("@solana/web3.js");
 const BN = require("bn.js");
 const { Wallet, AnchorProvider, Program } = require("@project-serum/anchor");
-const AmmImpl = require("@mercurial-finance/dynamic-amm-sdk");
 const { connection, wallet } = require("../../../helpers/config.js");
 const {PROGRAM_ID} = require("./constants.js")
+const {fetchDLMMPoolId} = require("./fetch-pool.js")
 
 console.log("PROGRAM_ID", PROGRAM_ID);
 const ourWallet = new Wallet(wallet);
@@ -12,6 +12,15 @@ const provider = new AnchorProvider(connection, Wallet, {
   commitment: "confirmed",
 });
 
-async function swapQuote(poolAddress, swapAmount, isSwapAtoB) {
+async function swap(tokenAddress){
+    const poolId = await fetchDLMMPoolId(tokenAddress);
+    const dlmmPool = await DLMM.create(connection, new PublicKey(poolId));
+    console.log(dlmmPool);
+
 
 }
+async function main(){
+    const tokenAddress = "7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr";
+    await swap(tokenAddress);
+}
+main();
