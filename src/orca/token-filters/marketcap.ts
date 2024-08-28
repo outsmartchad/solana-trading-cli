b8231595b4c7c56a6649f1f7d6d60ec6f3c65922
chiwangso <chiwang1712@gmail.com>
2024-08-28 18:09:12 +0800

    added method to fetch the market cap of a token

--- src/orca/token-filters/marketcap.ts
+++ src/orca/token-filters/marketcap.ts
@@ -1,6 +1,14 @@
 import {fetchWhirlPool} from "../Pool";
-
+import {getCurrentPriceInUSD} from "../fetch-price";
+import { PublicKey } from "@solana/web3.js";
+import { connection } from "../../helpers/config";
 export async function getCurrentMarketCap(token_address:string):Promise<any> {
-    const whirlPool:any = await fetchWhirlPool(token_address);
-    
+    const priceInUSD = await getCurrentPriceInUSD(token_address);
+    const tokenSupply: any = await connection.getTokenSupply(
+        new PublicKey(token_address)
+      );
+      const marketCap: number = priceInUSD * tokenSupply.value.uiAmount;
+      return marketCap;
 }
+
+//getCurrentMarketCap("7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr");
